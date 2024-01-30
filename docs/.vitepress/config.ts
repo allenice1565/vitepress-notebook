c29cb32186893c4fbe8b693c62664eb8ed6cf712
lxc <longxc@go-goal.com>
2024-01-30 15:00:02 +0800

    手动配置导航

--- docs/.vitepress/config.ts
+++ docs/.vitepress/config.ts
@@ -25,6 +25,52 @@ export default defineConfig({
                         text: "JavaScript",
                         link: "/pages/6cc245",
                     },
+                    {
+                        text: "TypeScript",
+                        link: "/pages/55cf2e",
+                    },
+                    {
+                        text: "Vue",
+                        link: "/pages/a51a57",
+                    },
+                    {
+                        text: "CSS",
+                        link: "/pages/17c8be",
+                    },
+                    {
+                        text: "构建",
+                        items: [
+                            {
+                                text: "Rollup",
+                                link: "/pages/f6535a",
+                            },
+                        ],
+                    },
+                    {
+                        text: "工具",
+                        link: "/pages/7046bb",
+                    },
+                    {
+                        text: "NodeJS",
+                        link: "/pages/4a2328",
+                    },
+                    {
+                        text: "遇见的问题",
+                        link: "/pages/c44a64",
+                    },
+                ],
+            },
+            {
+                text: "其他技术栈",
+                items: [
+                    {
+                        text: "Docker",
+                        link: "/pages/566963",
+                    },
+                    {
+                        text: "Python",
+                        link: "/pages/64ecaf",
+                    },
                 ],
             },
         ],
